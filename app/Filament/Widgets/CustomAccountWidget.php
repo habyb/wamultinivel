bd61373675c80eaed113c96f5800aa2b040d82df
Habyb Fernandes <habybfernandes@gmail.com>
2025-03-08 13:40:41 -0500

    updated from AccountWidget to CustomAccountWidget

---
+++ app/Filament/Widgets/CustomAccountWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\AccountWidget as BaseWidget;
+
+class CustomAccountWidget extends BaseWidget
+{
+    protected static string $view = 'vendor.filament.widgets.account-widget';
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -10,13 +10,13 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use App\Filament\Widgets\CustomAccountWidget;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -37,7 +37,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
+                CustomAccountWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
