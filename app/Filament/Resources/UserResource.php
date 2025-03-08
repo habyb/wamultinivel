ba9a642ac2f0d9bbc49600f41e296a65ec73e513
Habyb Fernandes <habybfernandes@gmail.com>
2025-03-08 16:29:11 -0500

    removed email_verified_at field from resource table

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -60,9 +60,6 @@ class UserResource extends Resource
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('email_verified_at')
-                    ->dateTime()
-                    ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
