540c5a30e296d02d1dc5c61089d06d56c2a89ed9
Habyb Fernandes <habybfernandes@gmail.com>
2025-04-18 15:17:29 -0500

    updated route redirect from / (root) to /admin

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InviteRedirectController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
 
 Route::get('/{codigo}', [InviteRedirectController::class, 'handle']);
