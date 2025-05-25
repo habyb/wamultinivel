c6545116107920038e4dbaa6ea071d36bcdc9af0
Habyb Fernandes <habybfernandes@gmail.com>
2025-05-24 21:51:41 -0500

    updated is_add_date_of_birth is true

--- app/Console/Commands/AssignEmbaixadorRoleToUsers.php
+++ app/Console/Commands/AssignEmbaixadorRoleToUsers.php
@@ -52,7 +52,7 @@ class AssignEmbaixadorRoleToUsers extends Command
             $users = User::role('Membro')->get();
 
             foreach ($users as $user) {
-                if ($user->firstLevelGuests()->where('is_add_email', true)->exists()) {
+                if ($user->firstLevelGuests()->where('is_add_date_of_birth', true)->exists()) {
                     // Remove current role Membro
                     $user->removeRole('Membro');
 
